4374efc095addc7221267b65374e6297997684f7
Pouya Ataei <pouya.ataei.7@gmail.com>
2019-09-18 22:19:38 +1200

    writing the first spy

--- src/test/CustomerForm.test.js
+++ src/test/CustomerForm.test.js
@@ -53,14 +53,18 @@ describe("CustomerForm", () => {
 
   const itSubmitsExistingValue = (fieldName, value) =>
     it("saves existing value when submitted", async () => {
-      expect.hasAssertions();
+      let submitArg;
+
       render(
         <CustomerForm
           {...{ [fieldName]: value }}
-          onSubmit={props => expect(props[fieldName]).toEqual(value)}
+          onSubmit={customer => (submitArg = customer)}
         />
       );
       await ReactTestUtils.Simulate.submit(form("customer"));
+
+      expect(submitArg).toBeDefined();
+      expect(submitArg[fieldName]).toEqual(value);
     });
 
   const itSubmitsNewValue = fieldName =>
