055e8c8f2c0a401d240ae529a1708c4cebbadd83
Pouya Ataei <pouya.ataei.7@gmail.com>
2019-10-10 22:24:10 +1300

    tested the initial state of the app

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
-<<<<<<< HEAD
-This is a pilot project!
-=======
-#TDD APOOINTMENT APP
-
-This is a rock-solid applicatio built by a perfectionist coder called Pouya. This guy writes code you've never seen before... LOL.
->>>>>>> c847b5349650e6eca653619f9028a1474c5c4b69
+This is a pilot project! wher are testing jsDOC, jest, enzyme and many more stuff here!
--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div data-test="component-app">
-      <h1 data-test="counter-display">The counter is currently </h1>
+      <h1 data-test="counter-display">
+        The counter is currently: <span data-test="counter">0</span>{" "}
+      </h1>
       <button data-test="increment-button">Increment counter</button>
     </div>
   );
